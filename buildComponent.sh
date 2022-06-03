d28f4632d783ca3b41f28816558a6cf80c35d51e
brendena <badamczak@creativemicro.com>
2022-06-03 13:06:49 -0400

    fixed the repo build script

--- buildComponent.sh
+++ buildComponent.sh
@@ -3,13 +3,12 @@ echo "building Math Equation component"
 
 cd MathEquations-component
 
-npm run build:lib
+npm run build
 
 mv build/direflowBundle.js ../Math_Equation_Component.html
 
 cd ..
 
 sed -i '1s/^/<script>/' Math_Equation_Component.html
-echo "</script>" >> Math_Equation_Component.html
+echo -e "\n</script>" >> Math_Equation_Component.html
 
-git add --all
